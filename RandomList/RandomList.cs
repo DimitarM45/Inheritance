deeb6e273552f3c1aafb197a44bda0ffe5014187
Dimitar <dimitar45minchev@gmail.com>
2022-10-25 21:25:10 +0300

    Add project files.

---
+++ RandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string RandomString()
+        {
+            int randomIndex = random.Next(0, Count);
+
+            string stringToReturn = this[randomIndex];
+
+            RemoveAt(randomIndex);
+
+            return stringToReturn;
+        }
+    }
+}
---
+++ StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var element in collection)
+                Push(element);
+        }
+    }
+}
